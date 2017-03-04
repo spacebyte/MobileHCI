1ea4042ab8a21619e59a39c4762e94e8829f3b24
2073036n <timothy.ness@yahoo.co.uk>
2017-03-04 16:19:24 +0000

    Had some git mare, now committing the changes mentioned in the last commit message

--- app/src/main/java/com/example/rachel/mhciproject/WorkoutProgressActivity.java
+++ app/src/main/java/com/example/rachel/mhciproject/WorkoutProgressActivity.java
@@ -1,7 +1,26 @@
 package com.example.rachel.mhciproject;
 
 import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.le.BluetoothLeScanner;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothManager;
+import android.bluetooth.le.ScanCallback;
+import android.bluetooth.le.ScanFilter;
+import android.bluetooth.le.ScanResult;
+import android.bluetooth.le.ScanSettings;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by jack on 01/03/2017.
@@ -9,9 +28,129 @@ import android.os.Bundle;
 
 public class WorkoutProgressActivity extends Activity {
 
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    private BluetoothAdapter bleDev = null;
+    private BluetoothLeScanner scanner = null;
+    private Button toggleScan = null;
+    private TextView progressText = null;
+
+    // request ID for enabling Bluetooth
+    private static final int REQUEST_ENABLE_BT = 1000;
+
+    private boolean isScanning = false;
+    private int scanMode = ScanSettings.SCAN_MODE_BALANCED;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_workout_progress);
+
+        progressText = (TextView) findViewById(R.id.progressText);
+
+        // set up a handler for taps on the start/stop scanning button
+        toggleScan = (Button) findViewById(R.id.btnToggleScan);
+        toggleScan.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                toggleScan();
+            }
+
+        });
+
+        // retrieve the BluetoothManager instance and check if Bluetooth is enabled. If not the
+        // user will be prompted to enable it and the response will be checked in onActivityResult
+        final BluetoothManager bluetoothManager = (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
+        bleDev = bluetoothManager.getAdapter();
+        if (bleDev == null || !bleDev.isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+        }
     }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode == REQUEST_ENABLE_BT) {
+            if(resultCode != RESULT_OK) {
+                Toast.makeText(this, "Bluetooth not enabled!", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "Bluetooth enabled successfully", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private void toggleScan() {
+        if(!isScanning)
+            startWorkout();
+        else
+            pauseWorkout();
+    }
+
+    private void pauseWorkout() {
+        if(scanner != null && isScanning) {
+            Toast.makeText(this, "Stopping BLE scan...", Toast.LENGTH_SHORT).show();
+            isScanning = false;
+            Log.i(TAG, "Scan stopped");
+            scanner.stopScan(bleScanCallback);
+        }
+
+        toggleScan.setText("Resume Workout");
+    }
+
+    private void startWorkout() {
+        toggleScan.setText("Pause Workout");
+        if(scanner == null) {
+            scanner = bleDev.getBluetoothLeScanner();
+            if(scanner == null) {
+                // probably tried to start a scan without granting Bluetooth permission
+                Toast.makeText(this, "Failed to start scan (BT permission granted?)", Toast.LENGTH_LONG).show();
+                Log.w(TAG, "Failed to get BLE scanner instance");
+                return;
+            }
+        }
+
+        Toast.makeText(this, "Starting BLE scan...", Toast.LENGTH_SHORT).show();
+
+        List<ScanFilter> filters = new ArrayList<>();
+        ScanSettings settings = new ScanSettings.Builder().setScanMode(scanMode).build();
+        scanner.startScan(bleScanCallback);
+        isScanning = true;
+    }
+
+    // class implementing BleScanner callbacks
+    private ScanCallback bleScanCallback = new ScanCallback() {
+
+        @Override
+        public void onScanResult(int callbackType, ScanResult result) {
+            super.onScanResult(callbackType, result);
+            progressText.setText("test1");
+
+            final BluetoothDevice dev = result.getDevice();
+
+            if(dev != null && isScanning) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        progressText.setText("something");
+                    }
+                });
+            }
+        }
+
+        @Override
+        public void onBatchScanResults(List<ScanResult> results) {
+            progressText.setText("batch...");
+            super.onBatchScanResults(results);
+            Log.d(TAG, "BatchScanResult(" + results.size() + " results)");
+        }
+
+        @Override
+        public void onScanFailed(int errorCode) {
+            super.onScanFailed(errorCode);
+            progressText.setText("failed...");
+            Log.w(TAG, "ScanFailed(" + errorCode + ")");
+        }
+    };
+
 }
