94f33fcc11401140d72530eff3efb19dc857196b
2073036n <timothy.ness@yahoo.co.uk>
2017-03-07 08:55:31 +0000

    added a timer for when the user is running and a number for the repetitions of workout excercises

--- app/src/main/java/com/example/rachel/mhciproject/WorkoutProgressActivity.java
+++ app/src/main/java/com/example/rachel/mhciproject/WorkoutProgressActivity.java
@@ -15,6 +15,7 @@ import android.bluetooth.BluetoothManager;
 import android.bluetooth.le.ScanCallback;
 import android.bluetooth.le.ScanResult;
 import android.bluetooth.le.ScanSettings;
+import android.os.Handler;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.util.Log;
@@ -41,6 +42,16 @@ public class WorkoutProgressActivity extends Activity {
     private ImageView activityImage = null;
     private ImageView nextActivityImage = null;
     private TextView upNext = null;
+    private boolean inBeaconRange = false;
+
+    private TextView tempTextView; //Temporary TextView
+    private Handler mHandler = new Handler();
+    private long startTime;
+    private long elapsedTime;
+    private final int REFRESH_RATE = 100;
+    private String hours,minutes,seconds,milliseconds;
+    private long secs,mins,hrs,msecs;
+    private boolean stopped = false;
 
     // request ID for enabling Bluetooth
     private static final int REQUEST_ENABLE_BT = 1000;
@@ -96,6 +107,9 @@ public class WorkoutProgressActivity extends Activity {
     }
 
     private void pauseWorkout() {
+        mHandler.removeCallbacks(startTimer);
+        stopped = true;
+
         if(scanner != null && isScanning) {
             Toast.makeText(this, "Stopping BLE scan...", Toast.LENGTH_SHORT).show();
             isScanning = false;
@@ -108,11 +122,35 @@ public class WorkoutProgressActivity extends Activity {
         progressText.setTextColor(Color.rgb(211,211,211));
     }
 
+    private Runnable startTimer = new Runnable() {
+        public void run() {
+            elapsedTime = System.currentTimeMillis() - startTime;
+            updateTimer(elapsedTime);
+            mHandler.postDelayed(this,REFRESH_RATE);
+        }
+    };
+
+    public void resetClick (View view){
+        stopped = false;
+        ((TextView)findViewById(R.id.timer)).setText("00:00:00");
+    }
+
     private void startWorkout() {
+        if(stopped){
+            startTime = System.currentTimeMillis() - elapsedTime;
+        }
+        else{
+            startTime = System.currentTimeMillis();
+        }
+        mHandler.removeCallbacks(startTimer);
+        mHandler.postDelayed(startTimer, 0);
+
+        ((TextView)findViewById(R.id.timer)).setVisibility(View.VISIBLE);
+
         toggleScan.setText("Pause Workout");
         activityImage.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.running));
         activityImage.setVisibility(View.VISIBLE);
-        progressText.setTextColor(Color.rgb(127,127,127));
+        progressText.setTextColor(Color.rgb(107,130,253));
         progressText.setVisibility(View.VISIBLE);
         upNext.setVisibility(View.VISIBLE);
         nextActivityImage.setVisibility(View.VISIBLE);
@@ -133,12 +171,28 @@ public class WorkoutProgressActivity extends Activity {
         isScanning = true;
     }
 
-    private void nextWorkout(){
-        activityImage.setImageDrawable(ContextCompat.getDrawable(this,R.drawable.push_up));
-        progressText.setText("Flat Pushup");
-        nextActivityImage.setImageDrawable(ContextCompat.getDrawable(this,R.drawable.running));
-        upNext.setText("Up Next: Running");
+    private void nextWorkout(String workout){
+        if(workout.equals("Running")){
+            activityImage.setImageDrawable(ContextCompat.getDrawable(this,R.drawable.running));
+            progressText.setText("Running");
+            stopped = false;
+            ((TextView)findViewById(R.id.timer)).setText("00:00:00");
+            mHandler.removeCallbacks(startTimer);
+            mHandler.postDelayed(startTimer, 0);
 
+            nextActivityImage.setImageDrawable(ContextCompat.getDrawable(this,R.drawable.push_up));
+            upNext.setText("Up Next: Flat Pushups");
+        }
+        else {
+            activityImage.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.push_up));
+            progressText.setText("Flat Pushup");
+            nextActivityImage.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.running));
+            upNext.setText("Up Next: Running");
+
+            mHandler.removeCallbacks(startTimer);
+            stopped = true;
+            ((TextView) findViewById(R.id.timer)).setText("10");
+        }
     }
 
     // class implementing BleScanner callbacks
@@ -155,7 +209,14 @@ public class WorkoutProgressActivity extends Activity {
                     @Override
                     public void run() {
                         if(dev.getName()!=null && dev.getName().equals("Kontakt 0k30")){
-                            nextWorkout();
+                            nextWorkout("Flat Pushup");
+                            inBeaconRange=true;
+                        }
+                        else{
+                            if(inBeaconRange){
+                                nextWorkout("Running");
+                                inBeaconRange=false;
+                            }
                         }
                     }
                 });
@@ -197,6 +258,61 @@ public class WorkoutProgressActivity extends Activity {
                 return;
             }
         }
+    }
+
+    private void updateTimer (float time){
+        secs = (long)(time/1000);
+        mins = (long)((time/1000)/60);
+        hrs = (long)(((time/1000)/60)/60);
+
+		/* Convert the seconds to String
+		 * and format to ensure it has
+		 * a leading zero when required
+		 */
+        secs = secs % 60;
+        seconds=String.valueOf(secs);
+        if(secs == 0){
+            seconds = "00";
+        }
+        if(secs <10 && secs > 0){
+            seconds = "0"+seconds;
+        }
+
+		/* Convert the minutes to String and format the String */
+
+        mins = mins % 60;
+        minutes=String.valueOf(mins);
+        if(mins == 0){
+            minutes = "00";
+        }
+        if(mins <10 && mins > 0){
+            minutes = "0"+minutes;
+        }
+
+        /* Convert the hours to String and format the String */
+
+        hours=String.valueOf(hrs);
+        if(hrs == 0){
+            hours = "00";
+        }
+        if(hrs <10 && hrs > 0){
+            hours = "0"+hours;
+        }
+
+    	/* Although we are not using milliseconds on the timer in this example
+    	 * I included the code in the event that you wanted to include it on your own
+    	 */
+        milliseconds = String.valueOf((long)time);
+        if(milliseconds.length()==2){
+            milliseconds = "0"+milliseconds;
+        }
+        if(milliseconds.length()<=1){
+            milliseconds = "00";
+        }
+        milliseconds = milliseconds.substring(milliseconds.length()-3, milliseconds.length()-2);
+
+		/* Setting the timer text to the elapsed time */
+        ((TextView)findViewById(R.id.timer)).setText(hours + ":" + minutes + ":" + seconds);
     }
 
 }
